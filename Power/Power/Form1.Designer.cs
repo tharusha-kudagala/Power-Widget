cb86f5366e8d100a1894561a16bd3c9c42aea57b
Tharusha Kudagala <44969527+tharusha-kudagala@users.noreply.github.com>
2019-07-16 11:47:46 +0530

    Add files via upload

---
+++ Power/Power/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Power
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Chartreuse;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "100%";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AC";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 20;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(88, 22);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Power/Power/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Power
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Left = 640;
+        }
+        int i = 0,x=0;
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(i%2 == 0)
+            {
+                bool pow = System.Windows.Forms.SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Offline;
+                if (pow == true)
+                {
+                    label2.Text = "DC";
+                    label2.BackColor = Color.Red; 
+                }
+                    
+                else
+                {
+                    label2.Text = "AC";
+                    label2.BackColor = Color.GreenYellow;
+                }
+            }
+            else
+            {
+                bool pow = System.Windows.Forms.SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Offline;
+                if (pow == true)
+                {
+                    label2.Text = "DC";
+                    label2.BackColor = Color.Red;
+                }
+                
+                else
+                {
+                    label2.Text = "AC";
+                    label2.BackColor = Color.GreenYellow;
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch(e.Button)
+            {
+                case MouseButtons.Left:
+                    this.WindowState = FormWindowState.Minimized;
+
+                    break;
+
+                case MouseButtons.Right:
+                    Close();
+                    break;
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (x % 2 == 0)
+            {
+                PowerStatus power = SystemInformation.PowerStatus;
+                int check = (int)(power.BatteryLifePercent * 100);
+                label1.Text = Convert.ToString(check) + "%";
+
+                if (check == 100)
+                    label1.Text = "Full";
+                else if (check >= 80)
+                    label1.BackColor = Color.Blue;
+                else if (check >= 50)
+                    label1.BackColor = Color.Yellow;
+                else if (check >= 30)
+                    label1.BackColor = Color.Orange;
+                else if (check >= 15)
+                    label1.BackColor = Color.OrangeRed;
+                else
+                    label1.BackColor = Color.Red;
+            }
+            else
+            {
+                PowerStatus power = SystemInformation.PowerStatus;
+                int check = (int)(power.BatteryLifePercent * 100);
+                label1.Text = Convert.ToString(check) + "%";
+
+                if (check == 100)
+                    label1.Text = "Full";
+                else if (check >= 80)
+                    label1.BackColor = Color.Blue;
+                else if (check >= 50)
+                    label1.BackColor = Color.Yellow;
+                else if (check >= 30)
+                    label1.BackColor = Color.Orange;
+                else if (check >= 15)
+                    label1.BackColor = Color.OrangeRed;
+                else
+                    label1.BackColor = Color.Red;
+            }
+        }
+    }
+}
